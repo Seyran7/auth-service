0c60c2a48519136f156fe76f97baaddef5fad1eb
seyran7 <brsr888@gmail.com>
2026-02-21 21:56:41 +0400

    feat:Add AuthService and AuthController

--- src/main/java/com/seyran/authservice/dto/RegisterRequest.java
+++ src/main/java/com/seyran/authservice/dto/RegisterRequest.java
@@ -2,12 +2,13 @@ package com.seyran.authservice.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class RegisterRequest {
     @Email
     @NotBlank
     private String email;
-
     @NotBlank
     private String password;
 }
---
+++ src/main/java/com/seyran/authservice/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.seyran.authservice.service;
+
+import com.seyran.authservice.dto.RegisterRequest;
+import com.seyran.authservice.entity.User;
+import com.seyran.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void register(RegisterRequest request){
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(bCryptPasswordEncoder.encode(request.getPassword()))
+                .build();
+        userRepository.save(user);
+
+    }
+}
