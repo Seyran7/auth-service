8c0b8368e21348419b9154e0a25ae252acdd4385
seyran7 <brsr888@gmail.com>
2026-02-22 22:29:22 +0400

    feat: implement register endpoint with password hashing and security config

--- src/main/java/com/seyran/authservice/config/SecurityConfig.java
+++ src/main/java/com/seyran/authservice/config/SecurityConfig.java
@@ -1,14 +1,33 @@
 package com.seyran.authservice.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/register")
+                        .permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
     }
-}
+
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder () {
+            return new BCryptPasswordEncoder();
+        }
+    }
+
