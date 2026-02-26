8cb8ab3e5750258337f715d1a5ed1ced0065d53c
seyran7 <brsr888@gmail.com>
2026-02-26 22:39:22 +0400

    Add TestContreller and improve SecurityConfig

--- src/main/java/com/seyran/authservice/config/SecurityConfig.java
+++ src/main/java/com/seyran/authservice/config/SecurityConfig.java
@@ -1,33 +1,45 @@
 package com.seyran.authservice.config;
 
 
+import com.seyran.authservice.service.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/register")
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
                         .permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
         @Bean
-        public BCryptPasswordEncoder bCryptPasswordEncoder () {
+        public PasswordEncoder bCryptPasswordEncoder () {
             return new BCryptPasswordEncoder();
+        }
+        @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception {
+        return config.getAuthenticationManager();
+
         }
     }
 
--- src/main/java/com/seyran/authservice/service/JwtAuthenticationFilter.java
+++ src/main/java/com/seyran/authservice/service/JwtAuthenticationFilter.java
@@ -2,19 +2,16 @@ package com.seyran.authservice.service;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.rmi.server.ServerCloneException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
