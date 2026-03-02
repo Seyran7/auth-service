739fc3851744841f2bc8830cb35eb375517546d7
seyran7 <brsr888@gmail.com>
2026-03-02 21:32:08 +0400

    Improve JwtService classes

--- src/main/java/com/seyran/authservice/service/JwtService.java
+++ src/main/java/com/seyran/authservice/service/JwtService.java
@@ -1,35 +1,51 @@
 package com.seyran.authservice.service;
 
+import com.seyran.authservice.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
-    private final String SECRET_KEY = "SECRET_KEY";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     private Key getSignKey(){
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        byte[] keyBytes;
+        return  Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
-    public String generateToken(String email){
+    public String generateAccessToken(User user){
         return Jwts.builder()
-                .setSubject(email)
+                .setSubject(user.getEmail())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*60))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
     public String extractEmail(String token){
-        Claims claims = Jwts.parserBuilder()
+
+        return extractAllClaims(token).getSubject();
+    }
+    public Boolean validateToken(String token, User user){
+        String email = extractEmail(token);
+        return email.equals(user.getEmail())&&!TokenExpired(token);
+    }
+    private boolean TokenExpired(String token){
+        return extractAllClaims(token).getExpiration().before(new Date());
+
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
                 .setSigningKey(getSignKey())
                 .build()
                 .parseClaimsJws(token).getBody();
-        return claims.getSubject();
-
     }
 }
