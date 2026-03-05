dd5fe1b3f99845ba81656a2384109b45c5440f67
seyran7 <brsr888@gmail.com>
2026-03-05 04:14:33 +0400

    feat: implement CustomUserDetailsService for authentication

---
+++ src/main/java/com/seyran/authservice/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.seyran.authservice.security;
+
+import com.seyran.authservice.entity.User;
+import com.seyran.authservice.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: "+email));
+        return new CustomUserDetail(user.getEmail(),user.getPassword());
+    }
+}
