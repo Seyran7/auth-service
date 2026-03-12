007292d4ef9323e93dde2399201fbdcc6b59788d
seyran7 <brsr888@gmail.com>
2026-03-12 18:31:44 +0400

    Add RefreshTokenRequest class

---
+++ src/main/java/com/seyran/authservice/dto/RefreshTokenRequest.java
@@ -0,0 +1,14 @@
+package com.seyran.authservice.dto;
+
+public class RefreshTokenRequest {
+
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
